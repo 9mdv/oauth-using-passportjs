1445cc2be078e863f0505aae834377c1d562d204
G <g11v8a@gmail.com>
2018-12-30 14:13:28 +0800

    updated keys

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,16 @@
 // add this file to .gitignore
 
 module.exports = {
-        google: {
-            clientID: '616581710231-3gh4r5mnbeadi0r9h1gbvk844kbir61o.apps.googleusercontent.com',
-            clientSecret: 'y3kKg0rZlk3WhE5u-A5aHwsD'
-        },
-        mongodb: {
-            dbURI: 'mongodb://iamshaunjp:test@ds151024.mlab.com:51024/oauth-test'
-        },
-        session: {
-            cookieKey: 'thenetninjaisawesomeiguess'
-        }
-};
+  google: {
+    clientID:
+      '484750281312-2b0anrlt6bcqmrs756l22v0i02g0vbcb.apps.googleusercontent.com',
+    clientSecret: 'B7vbb9NuDEH5eFB1wVBbgS51'
+  },
+  mongodb: {
+    dbURI:
+      'mongodb://g11:Zbbv3WBrNEGa2mmlYpsG@ds151955.mlab.com:51955/oauth-with-passport'
+  },
+  session: {
+    cookieKey: 'oauth-with-passport'
+  }
+}
